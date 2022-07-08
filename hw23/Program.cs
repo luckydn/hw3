9d0bda2ea29dcea1c3985d850566564792f8fcd3
luckydn <sp.donbass@gmail.com>
2022-07-08 16:00:34 +0300

    выполнил 23 задачу

---
+++ hw23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N, что бы программа отобразила таблицу кубов чисел от 1 до N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+double result;
+for (i=1; i<N; i++)
+    {
+        result= Math.Pow(i, 3);
+        Console.Write($"{result}, ");
+    }
+
+result = Math.Pow(i, 3);
+Console.WriteLine(result);

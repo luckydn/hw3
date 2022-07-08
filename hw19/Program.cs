cc7c0158d4cde650cf0dcb0fa2bc4f2eb93c7665
luckydn <sp.donbass@gmail.com>
2022-07-08 15:42:00 +0300

    выполнил 19 задачу

--- hw19/Program.cs
+++ hw19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число для определения, является ли оно палиндромом");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int An = n / 10000;
+int Bn = (n / 1000) % 10;
+int Cn = (n / 100) % 10;
+int Dn = (n / 10) % 10;
+int En = (n % 10);
+
+if(An == En)
+    {
+        if(Bn == Dn)
+        Console.WriteLine($"{n} --> Палиндром");
+    }
+else
+    {
+        Console.Write($"{n} --> Не является Палиндромом");
+    }

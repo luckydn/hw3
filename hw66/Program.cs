0cbaded55b5691b03f4bc7963aafa8b057fafaed
luckydn <sp.donbass@gmail.com>
2022-07-27 01:33:49 +0300

    66 задача

---
+++ hw66/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int sumNumberRecurse(int numberStart, int numberEnd)
+{
+    return numberEnd==numberStart?numberStart: numberEnd+sumNumberRecurse(numberStart, numberEnd-1);
+}
+
+
+Console.Write(sumNumberRecurse(1, 15));
